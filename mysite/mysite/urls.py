b0c9dbf22d9e82988d67bf298afa53ad61a86147
Serzh <lutovolk20@yandex.ru>
2023-02-18 15:34:02 +0300

    Created testapp1, added index_page func, created index.html

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from testapp1.views import index_page
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index_page)
 ]
